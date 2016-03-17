5e0fc12090182ea063999062c69cdd074e3ce8b7
Qipan Li <Qipan.Li@csr.com>
2016-03-17 16:11:54 +0800

    DP:Create: add singleton design pattern
    Signed-off-by: Qipan Li <swingboard@sina.com>

---
+++ create/singleton/singleton.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class singleton{
+public:
+	/*
+	 * it need to be the static method, otherwise it will produce compile error:
+	 * "error: cannot call member function ‘singleton* singleton::get_singleton()’ without object"
+	 * */
+	static class singleton *get_singleton()
+	{
+		if (!p)
+			p = new singleton();
+		return p;
+	}
+	void show_name()
+	{
+		cout << "my address:0x"<< p << " and my name is "<< p->name <<endl;
+	}
+	void set_name(string s)
+	{
+		p->name = s;	
+	}
+	~singleton()
+	{
+		delete p;
+		p = NULL;
+	}
+private:
+	static class singleton *p;
+	string name;
+	singleton()
+	{
+	}
+	
+};
+singleton *singleton::p = NULL;
+int main()
+{
+	singleton *ps = singleton::get_singleton();
+	ps->set_name("singleton1");
+	ps->show_name();
+	ps->set_name("singleton2");
+	ps->show_name();
+}
